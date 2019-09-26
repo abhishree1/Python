62ced527305b6cde648d384e9a3d1b9e79240148
Abhishree <abhishreekhangar01@gmail.com>
2019-09-26 23:35:21 +0530

    some basic about python

---
+++ basic.py
@@ -0,0 +1,6 @@
+print("I'll will be there for you!!!"*10);
+
+print(1+1)
+print(1*3)
+print(4/2)
+print(1<<2) #bit operator
---
+++ random_function.py
@@ -0,0 +1,11 @@
+import random
+
+anime=["Dragon ball series",
+		"Death Note",
+		"Naruto series",
+		"classroom assasin",
+		"Google it!!"]
+print("Which anime should i watch??")
+
+anim = random.choice(anime)
+print(f"You can watch {anim}")
