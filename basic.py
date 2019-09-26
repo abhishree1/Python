bc03be355ab5118c94a40a46bad590f9c42ffc5d
Abhishree <abhishreekhangar01@gmail.com>
2019-09-27 00:32:31 +0530

    Practiced some more concept

--- basic.py
+++ basic.py
@@ -4,3 +4,22 @@ print(1+1)
 print(1*3)
 print(4/2)
 print(1<<2) #bit operator
+
+print(int(23.0))
+print(float(23))
+
+print("""So no one told you life was gonna be this way
+Your job's a joke, you're broke
+Your love life's D.O.A
+It's like you're always stuck in second gear
+When it hasn't been your day, your week, your month
+Or even your year, but
+I'll be there for you
+(When the rain starts to pour)
+I'll be there for you
+(Like I've been there before)
+I'll be there for you
+('Cause you're there for me too)""")
+
+
+print("sdasda\"quote\"")
